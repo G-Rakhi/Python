15a1ac77757ffed164e730eca2b9f9da75bf675f
G-Rakhi <149416158+G-Rakhi@users.noreply.github.com>
2024-04-01 11:40:16 +0530

    Add files via upload

---
+++ Golden_Wires.py
@@ -0,0 +1,15 @@
+import heapq
+def find_cost(wires):
+    heapq.heapify(wires)
+    tot=0
+    while len(wires)>1:
+        w1=heapq.heappop(wires)
+        w2=heapq.heappop(wires)
+        tot+=(w1+w2)
+        heapq.heappush(wires,(w1+w2))
+    return tot
+   
+
+wires=list(map(int, input("enter list of wires:").split()))
+ans=find_cost(wires)
+print(ans)
